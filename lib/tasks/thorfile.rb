eb05a9d711f81608a29d44cb43d406492d45d8c8
Daniel Martin <etd-github@nomejortu.com>
2014-11-20 12:41:58 +0100

    Simplify Thor class structure

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,25 +1,40 @@
-class DradisTasks < Thor
-  class Upload < Thor
-    namespace     "dradis:upload"
+class NessusTasks < Thor
+include Core::Pro::ProjectScopedTask
+  namespace "dradis:plugins:nessus"
 
-    desc      "nessus FILE", "upload nessus results"
-    def nessus(file_path)
-      require 'config/environment'
+  desc "upload FILE", "upload Nessus v2 results (.nessus file)"
+  def upload(file_path)
+    require 'config/environment'
 
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::DEBUG
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG
 
-      unless File.exists?(file_path)
-        $stderr.puts "** the file [#{file_path}] does not exist"
-        exit -1
-      end
-
-      NessusUpload.import(
-        :file => file_path,
-        :logger => logger)
-
-      logger.close
+    unless File.exists?(file_path)
+      $stderr.puts "** the file [#{file_path}] does not exist"
+      exit -1
     end
 
+detect_and_set_project_scope
+
+    content_service = nil
+    template_service = nil
+    if defined?(Dradis::Pro)
+      content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Nessus)
+      template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Nessus)
+    else
+      content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Nessus)
+      template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Nessus)
+    end
+
+    importer = Dradis::Plugins::Nessus::Importer.new(
+                logger: logger,
+       content_service: content_service,
+      template_service: template_service
+    )
+
+    importer.import(file: file_path)
+
+    logger.close
   end
+
 end
