ac92aacbfdf87563ddd712112de24e792ad9bb24
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:49:30 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/nessus.rb
+++ lib/dradis/plugins/nessus.rb
@@ -9,20 +9,3 @@ require 'dradis/plugins/nessus/engine'
 require 'dradis/plugins/nessus/field_processor'
 require 'dradis/plugins/nessus/importer'
 require 'dradis/plugins/nessus/version'
-
-module Dradis
-  module Plugins
-    module Nessus
-
-      # This is required while we transition the Upload Manager to use
-      # Dradis::Plugins
-      module Meta
-        NAME = "Nessus output (.nessus) file upload"
-        EXPECTS = "Nessus XML (V2) format."
-        module VERSION
-          include Dradis::Plugins::Nessus::VERSION
-        end
-      end
-    end
-  end
-end
