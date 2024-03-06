71488ba5a72f03084e2b1caca1efd60bfad31de0
Caitlin <caitlinhenry.ss@gmail.com>
2024-03-06 15:28:07 -0500

    move default mappings from files to service

--- lib/dradis/plugins/nessus.rb
+++ lib/dradis/plugins/nessus.rb
@@ -7,5 +7,6 @@ end
 
 require 'dradis/plugins/nessus/engine'
 require 'dradis/plugins/nessus/field_processor'
+require 'dradis/plugins/nessus/mapping'
 require 'dradis/plugins/nessus/importer'
 require 'dradis/plugins/nessus/version'
